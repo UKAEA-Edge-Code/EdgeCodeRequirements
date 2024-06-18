26b52673de7f8282481a0ae607990e3dfc719902
Owen Parry <owen.parry@ukaea.uk>
2024-06-18 15:22:21 +0100

    Separate debug and release builds for examples.

--- dealII/examples/build_eg.sh
+++ dealII/examples/build_eg.sh
@@ -2,17 +2,19 @@
 
 this_dir=$( cd -- "$(realpath $( dirname -- "${BASH_SOURCE[0]}" ))" &> /dev/null && pwd )
 cmake_opts=""
+build_mode="Release"
 
 #==============================================================================
 build_eg() {
     # Build
     eg_dir="$this_dir/$1"
     if [ -d "$eg_dir" ]; then
-        cmake_cmd="cmake $cmake_opts -B build ."
+        build_dir="builds/$build_mode"
+        cmake_cmd="cmake $cmake_opts -B $build_dir ."
         cd "$eg_dir" &> /dev/null
         eval "$cmake_cmd"
         cd - &> /dev/null
-        cd "$eg_dir/build" &> /dev/null
+        cd "$eg_dir/$build_dir" &> /dev/null
         make
         #make run
         cd - &> /dev/null
@@ -30,10 +32,19 @@ set_cmake_options()
         echo "Couldn't find 'dealii' spack pkg"
         exit 1
     fi
-    cmake_opts="-DDEAL_II_DIR=$dealii_dir"
+    cmake_opts="-DDEAL_II_DIR=$dealii_dir -DCMAKE_BUILD_TYPE=$build_mode"
 }
 #==============================================================================
 
-set_cmake_options
+eg_name="$1"
 
-build_eg "$1"
+if [ $# -eq 2 ]; then
+    build_mode="$2"
+elif [ $# -eq 1 ]; then
+    build_mode="Release"
+else
+    echo "usage: build_eg.sh [eg_dir] <build_mode>"
+    exit 2
+fi
+set_cmake_options
+build_eg "$eg_name" "$build_mode"
